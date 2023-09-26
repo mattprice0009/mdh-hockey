0cac6f66ae12f6e425872e13d0c31ec2c780a8bf
mattprice0009 <42753079+mattprice0009@users.noreply.github.com>
2023-09-26 13:47:13 -0500

    Updated CAPFRIENDLY_GRAPH_URL_ROOT to shared url

--- mdhhockey/constants.py
+++ mdhhockey/constants.py
@@ -17,7 +17,7 @@ AZURE_SCOPES = ['https://graph.microsoft.com/.default']
 AZURE_AUTHORITY = 'https://login.microsoftonline.com/consumers'
 AZURE_TOKEN_CACHE = f'{CACHE_DIR}/cache.bin'
 AZURE_USER = config['azure']['user']
-CAPFRIENDLY_GRAPH_URL_ROOT = "https://graph.microsoft.com/v1.0/me/drive/root:/Hockey/MDH/MDH-CapFriendly.xlsx:/workbook/"
+CAPFRIENDLY_GRAPH_URL_ROOT = "https://graph.microsoft.com/v1.0/drives/56555516577eabf8/items/56555516577EABF8!64168/workbook"
 
 # Directory to store the Fantrax input CSV
 INPUTS_DIR = f'{PROJECT_ROOT}/inputs'
